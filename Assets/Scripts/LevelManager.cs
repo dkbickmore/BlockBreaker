4b1e796d6678c8cca356c8553af4914fa2048c29
dkbickmore <dkbickmore@gmail.com>
2016-02-22 12:36:55 -0700

    start

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log("Level requested: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest()
+	{
+		Debug.Log("End Requested");
+		Application.Quit();
+	}
+	
+
+}
